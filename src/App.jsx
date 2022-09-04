aad05608c2d3082df663a5fc686d186f157cb6c8
Lulão Spada <lucasspadoniferreira@gmail.com>
2022-09-04 19:31:13 -0300

    primeira tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import loginIMG from './assets/Login.png';
+import loginIMG from './assets/Subtract.png';
 import './styles.css';
 
 export const App = () => {
@@ -11,13 +11,13 @@ export const App = () => {
       <div className="container-login">
         <div className="wrap-login">
           <form className="login-form">
-
-            <span className="login-title">Bem Vindo!</span>
-
-            <span className="login-title">
-              <img src={ loginIMG } alt="Login" />
-            </span>
-
+              
+              <span className="login-title">
+                <img src={ loginIMG } alt="Login" />
+              </span>
+            
+            <span className="text-green1">CADASTRE-SE </span>
+            <span className='login-title'>OU FAÇA LOGIN</span>
             <div className="wrap-input">
               <input 
                 className={email !== "" ? 'has-val input' : 'input'}
@@ -25,7 +25,7 @@ export const App = () => {
                 value={email}
                 onChange={e => setEmail(e.target.value)}
               />
-              <span className="focus-input" data-placeholder="Email"></span>
+              <span className="focus-input" data-placeholder='EMAIL OU NOME DE USUÁRIO' ></span>
             </div>
 
             <div className="wrap-input">
@@ -35,17 +35,15 @@ export const App = () => {
                 value={password}
                 onChange={e => setpassword(e.target.value)}
               />
-              <span className="focus-input" data-placeholder="Password"></span>
+              <span className="focus-input" data-placeholder="INSIRA SUA SENHA"></span>
             </div>
 
             <div className="container-login-form-btn">
-              <button className="login-form-btn">Login</button>
+              <button className="login-form-btn">ENTRAR</button>
             </div>
 
             <div className="text-center">
-              <span className="txt1">Não possui conta?</span>
-
-              <a className="txt2" href="#">Criar conta</a>
+              <a className="txt2" href="#">OU CRIE SUA CONTA</a>
             </div>
 
           </form>
